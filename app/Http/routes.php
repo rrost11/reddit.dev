28b5e3844d0687045509ec404163497fe50dc6af
Rachel Rost <rachel.v.rost@gmail.com>
2016-10-14 14:31:40 -0500

    Updated routes - deleted unncessary routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -10,61 +9,11 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-//Laravel Main Page
-Route::get('/', 'HomeController@showWelcome');
-Route::get('/sayhello/{name}', function ($name) {
-	return "Hey Codeup Lassen Cohort!";
+Route::get('/', function () {
+    return redirect()->action('PostsController@index');
 });
-Route::get('/roll-dice/{guess}', 'HomeController@rollDice');
-Route::get('/uppercase/{word}', 'HomeController@upperCase');
-Route::get('/increment/{number}', 'HomeController@increment');
-Route::get('/add/{number1}/{number2}', function ($number1, $number2) {
-	return $number1 + $number2;
-});
-Route::get('/sayhello/{firstName}/{lastName}', function($firstName, $lastName)
-{
-    if ($firstName == "Chris") {
-        return Redirect::to('/');
-    }
-    $data = [
-    	'firstName' => $firstName,
-    	'lastName' => $lastName
-    ];
-    return view('my-first-view', $data);
-});
-Route::get('/rolldice/{guess}', 'HomeController@rollDice');
-
-//Post Controller
-Route::resource('posts', 'PostsController');
-Route::get('/posts', 'PostsController@index');
-Route::get('/posts/create', 'PostsController@create');
-Route::post('/posts', 'PostsController@store');
-Route::get('/posts/{post}', 'PostsController@show');
-Route::get('/posts/{post}/edit', 'PostsController@edit');
-Route::put('/posts/{post}', 'PostsController@update');
-Route::delete('/posts/{post}', "PostsController@destroy");
-
-
-
-//User Controller
 Route::resource('posts', 'PostsController');
 Route::resource('users', 'UsersController', ['except' => ['create', 'store']]);
-
-// Test
-Route::get('/orm-test', function(){
-    $post = new Post();
-    $post->created_by = 1;
-    $post->title = 'Eloquent is awsome';
-    $post->url = 'codeup.com';
-    $post->content = 'some content stuff';
-    $post->save();
-    $post2 = new post();
-    $post2->created_by = 1;
-    $post2->title = 'title';
-    $post2->url = 'google.com';
-    $post2->content = 'some content stuff';
-    $post2->save();
-});
 
 // Authentication routes...
 Route::get('auth/login', 'Auth\AuthController@getLogin');
