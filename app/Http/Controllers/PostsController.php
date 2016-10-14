7d682d4403df1ea20b204b8976363e912c568afd
Rachel Rost <rachel.v.rost@gmail.com>
2016-10-14 14:32:03 -0500

    Updated Users and Posts Controllers

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,11 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+
 use App\Models\Post;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
-
-
 
 class PostsController extends Controller
 {
@@ -16,18 +15,12 @@ class PostsController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-  
     public function index()
     {
-       
         $posts = Post::paginate(20);
-        
-        $data = ['posts'=>$posts];
-
+        $data = ['posts' => $posts];
         return view('posts.index')->with($data);
     }
-
-
 
     /**
      * Show the form for creating a new resource.
@@ -47,27 +40,25 @@ class PostsController extends Controller
      */
     public function store(Request $request)
     {
-        // associative array whose keys are input names in the request
-        // values are the validation rules
-        $rules = [
-            'title'   => 'required|min:5',
-            'url'     => 'required',
-            'content' => 'required',
-        ];
-        // will redirect back with $errors object populated if validation fails
-        
-        $request->session()->flash('ERROR_MESSAGE', 'Post was not saved. Please see messages under inputs.');
+        $rules = array(
+            'title'     => 'required|max:100',
+            'url'       => 'required',
+            'content'   => 'required',
+        );
+
+        $request->session()->flash('ERROR_MESSAGE', 'Post was not saved. Please see messages under inputs');
         $this->validate($request, $rules);
-        $post = new Post();
+        $request->session()->forget('ERROR_MESSAGE');
+
+        $post = new Post;
         $post->created_by = 1;
         $post->title = $request->title;
         $post->url = $request->url;
         $post->content = $request->content;
         $post->save();
 
-        
-        $request->session()->flash('SUCCESS_MESSAGE', 'Post was successfuly saved');
-        
+        $request->session()->flash('SUCCESS_MESSAGE', 'Post was saved successfully');
+
         return redirect()->action('PostsController@show', $post->id);
     }
 
@@ -78,11 +69,12 @@ class PostsController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function show($id)
-     {
+    {
         $post = Post::find($id);
-        $data = array ('post'=>$post);
-        return view ('posts.show')->with($data);
+        $data = ['post' => $post];
+        return view('posts.show')->with($data);
     }
+
     /**
      * Show the form for editing the specified resource.
      *
@@ -92,9 +84,10 @@ class PostsController extends Controller
     public function edit($id)
     {
         $post = Post::find($id);
-        $data = array('post' => $post);
+        $data = ['post' => $post];
         return view('posts.edit')->with($data);
     }
+
     /**
      * Update the specified resource in storage.
      *
@@ -104,23 +97,31 @@ class PostsController extends Controller
      */
     public function update(Request $request, $id)
     {
+        $rules = array(
+            'title'     => 'required|max:100',
+            'url'       => 'required',
+            'content'   => 'required',
+        );
+
+        $this->validate($request, $rules);
+
         $post = Post::find($id);
         $post->title = $request->title;
         $post->url = $request->url;
         $post->content = $request->content;
         $post->save();
+
         return redirect()->action('PostsController@show', $post->id);
     }
+
     /**
      * Remove the specified resource from storage.
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Request $request, $id)
+    public function destroy($id)
     {
-        return 'Deletes a post';
+        return 'destroy';
     }
-
-
 }
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,8 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-use App\User;
-
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
@@ -19,7 +17,7 @@ class UsersController extends Controller
     public function index()
     {
         $users = User::all();
-        $data['users'] = $users;
+        $data = ['users' => $users];
         return view('users.index')->with($data);
     }
 
@@ -41,16 +39,7 @@ class UsersController extends Controller
      */
     public function store(Request $request)
     {
-        
-        $this->validate($request,User::$rules);
-        $user = new User();
-
-        $user->name= $request->get('name');
-        $user->email= $request->get('email');
-        $user->password= $request->get('password');
-        $user->save();
-        
-        return redirect()->action('UsersController@index');
+        //
     }
 
     /**
@@ -62,7 +51,7 @@ class UsersController extends Controller
     public function show($id)
     {
         $user = User::find($id);
-        $data['user'] = $user;
+        $data = ['user' => $user];
         return view('users.show')->with($data);
     }
 
@@ -75,7 +64,7 @@ class UsersController extends Controller
     public function edit($id)
     {
         $user = User::find($id);
-        $data['user'] = $user;
+        $data = ['user' => $user];
         return view('users.edit')->with($data);
     }
 
@@ -88,16 +77,7 @@ class UsersController extends Controller
      */
     public function update(Request $request, $id)
     {
-        $this->validate($request,User::$rules);
-
-        $user = User::find($id);
-        $user->name= $request->get('name');
-        $user->email= $request->get('email');
-        $user->password= $request->get('password');
-        $user->save();
-
-        // return redirect()->action('usersController@index');
-        return redirect()->action('UsersController@show', $user->id);
+        $hashed_password = Hash::make($password);
     }
 
     /**
@@ -108,10 +88,6 @@ class UsersController extends Controller
      */
     public function destroy($id)
     {
-        
-        $user = User::find($id);
-        
-        $user->delete();
-        return redirect()->action('UsersController@index');
+        //
     }
 }
