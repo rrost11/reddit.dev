63207a82fcc307ec728bef83bedefbe10fc1f949
Rachel Rost <rachel.v.rost@gmail.com>
2016-10-14 14:21:30 -0500

    Added Auth Login/Logout/Registration Routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -65,4 +65,11 @@ Route::get('/orm-test', function(){
     $post2->save();
 });
 
+// Authentication routes...
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
 
+// Registration routes...
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
