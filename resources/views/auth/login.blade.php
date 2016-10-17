a1c03f1f2a1cc2739c9f13b1fcc757289b4e71e3
Rachel Rost <rachel.v.rost@gmail.com>
2016-10-17 16:13:25 -0500

    Updated Login View

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,10 +9,20 @@
 			    	<label for="email">Email:</label>
 			    	<input type="text" class="form-control" name="email" value="{{ old('email') }}">
 			  	</div>
+			  	@if($errors->has('email'))
+					<div class="alert alert-danger">
+						{{$errors->first('email')}}
+					</div>
+				@endif
 				<div class="form-group">
 			    	<label for="password">Password:</label>
 			    	<input type="password" class="form-control" name="password">
 			  	</div>
+			  	@if($errors->has('password'))
+					<div class="alert alert-danger">
+						{{$errors->first('password')}}
+					</div>
+				@endif
 			  	<button type="submit" class="btn btn-success">Login</button>
 			</form>
 		</div>
